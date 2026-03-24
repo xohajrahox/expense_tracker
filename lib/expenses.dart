3fe94bd6aa85421b1a7278371984e52b2ca0476a
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-03-24 12:16:41 -0400

    Created and Stubbed out Expense

---
+++ lib/expenses.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+
+}
+class _ExpensesState extends State<Expenses>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("CHART GOES HERE"),
+          SizedBox(height:30),
+          Text("EXPENSES List GOES HERE")
+        ],
+      ),
+    );
+  }
+  
+}
