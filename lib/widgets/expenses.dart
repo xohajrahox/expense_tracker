d7b52bc940912753f148f1d86c8fa74dfb252a0e
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-03-26 12:11:37 -0400

    Put scrollable list view with expenses on screen

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/Expense.dart';
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -31,8 +32,9 @@ class _ExpensesState extends State<Expenses>{
       body: Column(
         children: [
           Text("CHART GOES HERE"),
-          SizedBox(height:30),
-          Text("EXPENSES List GOES HERE")
+          SizedBox(
+            height:300,
+            child: ExpensesList(expenses:_registeredExpenses)),
         ],
       ),
     );
