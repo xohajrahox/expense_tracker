d2f729cab478537a8566156f4ae2de93f9f412da
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-04-09 11:18:43 -0400

    Changed dropdown for category to be selectable

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 final formatter = DateFormat.yMd();
@@ -13,6 +14,7 @@ class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -77,6 +79,23 @@ class _NewExpenseState extends State<NewExpense>{
             ],
           ),
           Row(children: [
+            DropdownButton(
+              value: _selectedCategory,
+              items: Category.values.map(
+                (category) => DropdownMenuItem(
+                  value: category,
+                  child: Text(category.name.toUpperCase(),),
+                ),
+              ).toList(),
+              onChanged: (value){
+                if( value == null)
+                {
+                  return;
+                }
+                setState((){
+                  _selectedCategory = value;
+                });
+              }),
             ElevatedButton(onPressed: (){
               Navigator.pop(context);
             }, child: Text("Cancel")),
