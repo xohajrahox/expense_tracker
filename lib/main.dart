fdfcff0b5eb76d9c745dea0d41c61709c4167754
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-04-14 12:13:01 -0400

    Started Themeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
-  ));
-}
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
 
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 20,
+          ),
+        ),
+        colorScheme: kColorScheme, 
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer, 
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
+}
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -14,9 +14,15 @@ class ExpenseItem extends StatelessWidget {
           vertical: 16,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),//Text box at the top of the column
-            const SizedBox(height:4),//a little space between first row of the column and the second
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),//Text box at the top of the column
+            const SizedBox(
+              height: 4,
+            ),//a little space between first row of the column and the second
             Row(children: [ //We need a bunch of things in this row of the column, so add a Row
               Text('\$${expense.amount.toStringAsFixed(2)}'), //amount first
               Spacer(),//spacer to push everything else over to the right
