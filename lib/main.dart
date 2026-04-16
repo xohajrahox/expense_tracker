dd4aba058dc96a7ec499097979ecf7fee7a97b48
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-04-16 11:32:32 -0400

    Finished Darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 ColorScheme kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125)
+);
 
 void main() {
   runApp(
@@ -31,6 +35,20 @@ void main() {
           ),
         ),
       ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
