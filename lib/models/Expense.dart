c432a010643d1d781a46da85dad2077620148096
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-04-16 11:51:13 -0400

    Added chart

--- lib/models/Expense.dart
+++ lib/models/Expense.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+
 const uuid = Uuid(); //making the uuid class
 final formatter = DateFormat.yMd();
+
 enum Category{food, travel, leisure, work}
 const categoryIcons={
   Category.food: Icons.lunch_dining,
@@ -11,7 +13,8 @@ const categoryIcons={
   Category.work: Icons.work,
 };
 class Expense{
-  Expense ({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
+  Expense ({required this.title, required this.amount, 
+  required this.date, required this.category}) : id = uuid.v4();
 
   final String id;
   final String title;
@@ -21,5 +24,25 @@ class Expense{
 
   String get formattedDate{
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+  : expenses = allExpenses
+        .where((expense) => expense.category == category)
+        .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -92,7 +93,7 @@ class _ExpensesState extends State<Expenses>{
       ),
       body: Column(
         children: [
-          Text("CHART GOES HERE"),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
